dae5e0e86667b01dca1b43646771dcd319d09809
ottodpc <cyprienotto@outlook.fr>
2022-06-05 23:41:12 +0200

    threejs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import styled from "styled-components";
 
-function App() {
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+import { Canvas } from "@react-three/fiber";
+import Box from "./components/Box";
+import { OrbitControls } from "@react-three/drei";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="App">
+      <Background />
+      <TextSection />
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Box />
+      </Canvas>
+    </Wrapper>
   );
 }
 
-export default App;
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+`;
